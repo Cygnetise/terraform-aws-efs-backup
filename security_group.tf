f294a05eab58f6261249eebddfbeaed4f00c478d
DangerDawson <david.dawson@gmail.com>
2020-02-28 16:07:17 +0000

    remove ssh access

--- security_group.tf
+++ security_group.tf
@@ -5,13 +5,6 @@ resource "aws_security_group" "datapipeline" {
   name        = module.label.id
   description = module.label.id
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   egress {
     from_port   = 0
     to_port     = 0
